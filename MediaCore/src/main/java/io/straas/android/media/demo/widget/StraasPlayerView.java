4acd7cd7caf5e11829f0f3d3757e0560f096b452
jason.chuang <jason.chuang@ikala.tv>
2018-06-19 08:36:58 +0800

    fix: update ui for post-roll

--- MediaCore/src/main/java/io/straas/android/media/demo/widget/StraasPlayerView.java
+++ MediaCore/src/main/java/io/straas/android/media/demo/widget/StraasPlayerView.java
@@ -472,6 +472,7 @@ public final class StraasPlayerView extends FrameLayout implements StraasMediaCo
                         break;
                     case PlaybackStateCompat.STATE_PLAYING:
                         mColumnAdPlay.setVisibility(GONE);
+                        mCanToggleControllerUi = false;
                         break;
                     case PlaybackStateCompat.STATE_PAUSED:
                         mColumnAdPlay.setVisibility(VISIBLE);
@@ -526,6 +527,7 @@ public final class StraasPlayerView extends FrameLayout implements StraasMediaCo
                             setKeepScreenOn(false);
                         }
                     case PlaybackStateCompat.STATE_STOPPED:
+                        mCanToggleControllerUi = true;
                         if (mIsLive) {
                             Bundle liveBundle = (mLiveBundle != null) ? mLiveBundle :
                                     getMediaControllerCompat().getExtras();
