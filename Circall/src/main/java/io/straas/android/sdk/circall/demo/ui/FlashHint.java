e68271a881b5a668d7bd9ed1cc58d5b5db628770
ItsRay <rayaya0425@gmail.com>
2018-11-30 15:34:19 +0800

    feat: Use built-in Handler in FlashHint.

--- Circall/src/main/java/io/straas/android/sdk/circall/demo/ui/FlashHint.java
+++ Circall/src/main/java/io/straas/android/sdk/circall/demo/ui/FlashHint.java
@@ -2,55 +2,30 @@ package io.straas.android.sdk.circall.demo.ui;
 
 import android.content.Context;
 import android.databinding.BindingAdapter;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-import java.lang.ref.WeakReference;
-
-import static io.straas.android.sdk.circall.demo.ui.FlashHint.FlashHandler.FLASH_INTERVAL;
-import static io.straas.android.sdk.circall.demo.ui.FlashHint.FlashHandler.MESSAGE_FLASH;
-
 public class FlashHint extends android.support.v7.widget.AppCompatImageView {
 
-    private FlashHandler mHandler;
+    static final long FLASH_INTERVAL = 1000;
+
     public boolean mFlashEnabled;
 
-    static class FlashHandler extends Handler {
-        static final long FLASH_INTERVAL = 1000;
-
-        static final int MESSAGE_FLASH = 0;
-
-        private final WeakReference<FlashHint> mRecordingFlashHint;
-
-        FlashHandler(FlashHint recordingFlashHint) {
-            super();
-            mRecordingFlashHint = new WeakReference<>(recordingFlashHint);
-        }
-
+    Runnable mRunFlash = new Runnable() {
         @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            FlashHint flashHint = mRecordingFlashHint.get();
-            if (flashHint == null) {
+        public void run() {
+            removeCallbacks(this);
+            if (!mFlashEnabled) {
                 return;
             }
-            switch (msg.what) {
-                case MESSAGE_FLASH:
-                    flashHint.setVisibility(
-                            flashHint.getVisibility() == VISIBLE
+            FlashHint.this.setVisibility(
+                    FlashHint.this.getVisibility() == VISIBLE
                             ? INVISIBLE
                             : VISIBLE);
-                    removeMessages(MESSAGE_FLASH);
-                    sendEmptyMessageDelayed(MESSAGE_FLASH, FLASH_INTERVAL);
-                    break;
-                default:
-                    break;
-            }
+            postDelayed(this, FLASH_INTERVAL);
         }
-    }
+    };
 
     public FlashHint(Context context) {
         this(context, null);
@@ -67,18 +42,17 @@ public class FlashHint extends android.support.v7.widget.AppCompatImageView {
 
     private void init() {
         setVisibility(View.GONE);
-        mHandler = new FlashHandler(this);
     }
 
     public void setFlash(boolean enabled) {
         mFlashEnabled = enabled;
         if (mFlashEnabled) {
             bringToFront();
-            mHandler.removeMessages(MESSAGE_FLASH);
-            mHandler.sendEmptyMessageDelayed(MESSAGE_FLASH, FLASH_INTERVAL);
+            removeCallbacks(mRunFlash);
+            postDelayed(mRunFlash, FLASH_INTERVAL);
         } else {
             setVisibility(View.INVISIBLE);
-            mHandler.removeMessages(MESSAGE_FLASH);
+            removeCallbacks(mRunFlash);
         }
     }
 
